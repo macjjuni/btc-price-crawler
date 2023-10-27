ab0cae78482cbb0a5af209666d93b02ef842ae1c
macjjuni <macjjuni@gmail.com>
2023-10-27 23:43:08 +0900

    Fix: 포트 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MvrvModule } from './mvrv/mvrv.module';
+import { TestController } from './test/test.controller';
+import { TestService } from './test/test.service';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [MvrvModule],
+  imports: [MvrvModule, TestModule],
+  controllers: [TestController],
+  providers: [TestService],
   // controllers: [],
   // providers: [],
 })
