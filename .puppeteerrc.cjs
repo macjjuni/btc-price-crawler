21b9c859d37bd48f4d9ce16a2de379a85d109d95
macjjuni <macjjuni@gmail.com>
2023-10-29 17:02:02 +0900

    Feat: 개발모드 오류 수정

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,8 +1,13 @@
 const { join } = require('path');
+const dotenv = require('dotenv');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 /**
  * @type {import("puppeteer").Configuration}
  */
+
+if (isDev) module.exports = {};
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
